1f67ee2ae0168157f25db73166b21405d256853a
ashwin <ashu61998@gmail.com>
2020-12-02 21:51:50 +0530

    css updated

--- js/modules/TheDataMiner.js
+++ js/modules/TheDataMiner.js
@@ -9,7 +9,7 @@ async function fetchData(sourceURL) {
         // ask for a resource, and then do something with it when it resolves
     let resource = await fetch(sourceURL).then(response => {
         if (response.status !== 200) {
-            alert(`Danger .... Error ${response.status}: ${errorCodes[response.status]}`);
+            alert(`Error ${response.status}: ${errorCodes[response.status]}`);
         } 
         
         return response;           
